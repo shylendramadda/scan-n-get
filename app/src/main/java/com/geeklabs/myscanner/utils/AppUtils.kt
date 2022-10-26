e8ddaafbab5fa1f59b7e9ba9d88d7541a3c59a20
Shailendra Madda <shylendramadda@gmail.com>
2022-10-26 20:51:47 +0530

    Update the dialog button position to start

--- app/src/main/java/com/geeklabs/myscanner/utils/AppUtils.kt
+++ app/src/main/java/com/geeklabs/myscanner/utils/AppUtils.kt
@@ -1,9 +1,9 @@
 package com.geeklabs.myscanner.utils
 
-import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.geeklabs.myscanner.R
 import java.text.SimpleDateFormat
@@ -34,12 +34,15 @@ object AppUtils {
                 listener.onNegativeClicked()
             }
         val dialog = dialogBuilder.create()
-        dialog.setCancelable(false)
-        dialog.setCanceledOnTouchOutside(false)
+        dialog.setCancelable(true)
+        dialog.setCanceledOnTouchOutside(true)
         dialog.show()
         val buttonPositive = dialog.getButton(DialogInterface.BUTTON_POSITIVE)
         val buttonNegative = dialog.getButton(DialogInterface.BUTTON_NEGATIVE)
         val textColor = ContextCompat.getColor(context, R.color.blue)
+        val positiveButtonLL = buttonPositive.layoutParams as LinearLayout.LayoutParams
+        positiveButtonLL.weight = 10f
+        buttonPositive.layoutParams = positiveButtonLL
         buttonPositive.setTextColor(textColor)
         buttonNegative.setTextColor(textColor)
         return dialog
