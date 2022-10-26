e0695454e0473ce21e56b30596040f251f5dd97b
Shailendra Madda <shylendramadda@gmail.com>
2022-10-26 15:16:26 +0530

    Add export as CSV option

---
+++ app/src/main/java/com/geeklabs/myscanner/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.geeklabs.myscanner.utils
+
+object Constants {
+}
---
+++ app/src/main/java/com/geeklabs/myscanner/utils/PermissionUtils.kt
@@ -0,0 +1,60 @@
+package com.tfl.qinspect.utils
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.*
+
+class PermissionUtils(val activity: Activity) {
+
+    private var permissionsList = arrayListOf<String>()
+
+    /*fun isPermissionGranted(permission: String): Boolean {
+        val result = activity.checkCallingOrSelfPermission(permission)
+        return result == PackageManager.PERMISSION_GRANTED
+    }*/
+
+    fun requestAllMandatoryPermissions() {
+        permissionsList = ArrayList<String>()
+        addPermissionIfNotGranted(Manifest.permission.READ_EXTERNAL_STORAGE)
+        addPermissionIfNotGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        addPermissionIfNotGranted(Manifest.permission.CAMERA)
+        askRequiredPermissions(REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS)
+//        addPermissionIfNotGranted(Manifest.permission.CALL_PHONE)
+//        addPermissionIfNotGranted(Manifest.permission.READ_PHONE_STATE)
+        addPermissionIfNotGranted(Manifest.permission.RECORD_AUDIO)
+    }
+
+    private fun addPermissionIfNotGranted(permission: String) {
+        if (ContextCompat.checkSelfPermission(activity, permission) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsList.add(permission)
+            ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)
+        }
+    }
+
+    private fun askRequiredPermissions(requestCode: Int) {
+        if (permissionsList.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                activity, permissionsList.toTypedArray(),
+                requestCode
+            )
+        }
+    }
+
+    fun checkLocationPermissions() : Boolean {
+        permissionsList = ArrayList<String>()
+        addPermissionIfNotGranted(Manifest.permission.ACCESS_FINE_LOCATION)
+        addPermissionIfNotGranted(Manifest.permission.ACCESS_COARSE_LOCATION)
+        askRequiredPermissions(REQUEST_CODE_LOCATION_PERMISSIONS)
+        return permissionsList.isEmpty()
+    }
+
+    companion object {
+        const val REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 1
+        const val REQUEST_CODE_LOCATION_PERMISSIONS = 2
+    }
+}
